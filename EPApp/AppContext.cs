e06e92b20675c4b347a680eb1c07ae491a574b87
boringhater <apmolokanova@gmail.com>
2022-05-26 19:54:58 +0300

    PDB_LAB6

---
+++ EPApp/AppContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPApp
+{
+    public class AppContext :DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptions)
+        {
+            dbContextOptions.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+
+        public void ShowDbContents()
+        {
+            var users = this.Users.ToList();
+            Console.WriteLine("Current database contents:");
+            foreach (User user in users)
+            {
+                Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+            }
+        }
+
+        public void ClearDb()
+        {
+            this.Users.RemoveRange(Users.Where(i => true));
+            this.SaveChanges();
+        }
+    }
+}
---
+++ EPApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace EPApp
+{
+    class Program
+    {
+        
+
+        public static void Main(string[] Args)
+        {
+            using(AppContext db = new AppContext())
+            {
+                db.Users.Add(new User { Name = "King", Age = 56 });
+                db.Users.Add(new User { Name = "Korchhar", Age = 44 });
+                db.SaveChanges();
+
+                db.ShowDbContents();
+
+                db.Users.Remove((User)db.Users.Where(user => user.Name == "Korchhar").First());
+                db.SaveChanges();
+
+                db.ShowDbContents();
+
+                User someUser = db.Users.ToList().First();
+                someUser.Age = 99;
+                db.Users.Update(someUser);
+
+                db.ShowDbContents();
+
+            }
+        }
+    }
+}
